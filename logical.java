5cf95565cff5a0fbadd81cdf2cedd873e2141afe
naveen9979 <sathyapriyak2020@gmail.com>
2024-08-06 09:35:38 -0700

    Add files via upload

---
+++ logical.java
@@ -0,0 +1,23 @@
+public class logical {
+    public static void main(String[]args){
+        int x = 10;
+        int y = 20;
+        int z = 15;
+        if (x < y && z > x)
+        // if x is less than y and z is greater than x
+        {
+             System.out.println("Both conditions are true.");
+        }
+    
+        
+        int a = 10;
+        System.out.println(a > 3 || a < 4);
+
+
+        int N = 5;
+        System.out.println(! (N > 5 && N < 10) );
+    
+        }
+    }
+    
+
